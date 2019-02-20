1e4d20f898c28be07bc15a9c6263a0c95c407a3b
MacOS13 <macos13@MacBook-Pro-cua-MacOS13.local>
2019-02-20 22:56:03 +0700

    fix deploy

--- deploy/deploy_server.sh
+++ deploy/deploy_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo cp -fr usr/local/nginx/conf/config.d/virtual.conf /usr/local/nginx/conf/config.d/
+sudo cp -fr usr/local/nginx/conf/conf.d/virtual.conf /usr/local/nginx/conf/conf.d/
 sudo mkdir /usr/local/nginx/snippets
 sudo cp -fr usr/local/nginx/snippets/letsencrypt.conf /usr/local/nginx/snippets
 sudo cp -fr usr/local/nginx/snippets/ssl.conf /usr/local/nginx/snippets
