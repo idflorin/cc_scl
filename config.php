b6e008d91497b1c8b9438602c12ca8b5cd670133
MacOS13 <macos13@MacBook-Pro-cua-MacOS13.local>
2019-02-20 22:26:49 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "cungcap_user";
+// MySQL Database Password
+$sql_db_pass = "cungcap_pass";
+// MySQL Database Name
+$sql_db_name = "cungcap";
+
+// Site URL
+$site_url = "http://cungcap.local"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "ca5cccf1-c3d1-4153-aa51-916d50082420"; // Your purchase code, don't give it to anyone. 
+?>
