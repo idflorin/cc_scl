5f78f574344609ab7267e7f91fdc8b95f82df6af
MacOS13 <macos13@MacBook-Pro-cua-MacOS13.local>
2019-02-21 00:20:33 +0700

    fix deploy

---
+++ deploy/deploy_server_local.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo cp -fr usr/local/nginx/conf/conf.d/virtual_local.conf /usr/local/nginx/conf/conf.d/
+sudo mkdir /usr/local/nginx/snippets
+sudo cp -fr usr/local/nginx/snippets/letsencrypt.conf /usr/local/nginx/snippets
+sudo cp -fr usr/local/nginx/snippets/ssl.conf /usr/local/nginx/snippets
+sudo yum install git
